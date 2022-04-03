fa5b6f8e379092cb4d3a31234f8800af666a1dae
Sultanbek <soultouchka@gmail.com>
2022-04-04 00:13:47 +0600

    feat(docker): added project name

--- docker/install_afisha_api.sh
+++ docker/install_afisha_api.sh
@@ -2,6 +2,7 @@
 docker-compose exec -T afisha-api-php-fpm chmod -R 777 storage/ bootstrap/
 docker-compose exec -T afisha-api-php-fpm cp .env.example .env
 docker-compose exec -T afisha-api-php-fpm composer install
+docker-compose exec -T afisha-api-php-fpm php artisan optimize:clear
 docker-compose exec -T afisha-api-php-fpm php artisan key:generate
 docker-compose exec -T afisha-api-php-fpm php artisan config:cache
 docker-compose exec -T afisha-api-php-fpm php artisan migrate:fresh --seed
